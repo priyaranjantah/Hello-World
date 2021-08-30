2230fe220c4d7f5d40a45aabc0915c391622962b
priyaranjantah <priyaranjantah@gmail.com>
2021-08-30 23:00:18 +0530

    updated

--- src/test/java/entrypoint/ChromeExten.java
+++ src/test/java/entrypoint/ChromeExten.java
@@ -23,7 +23,7 @@ public class ChromeExten {
 
     @BeforeClass
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
         chromeOptions.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
