fdeec1bec2bc180e1ee6e54a4c3cbf939cd598ca
priyaranjantah <priyaranjantah@gmail.com>
2021-08-30 22:27:22 +0530

    updated code

--- src/test/java/entrypoint/ChromeExten.java
+++ src/test/java/entrypoint/ChromeExten.java
@@ -20,14 +20,15 @@ public class ChromeExten {
     @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.silentOutput", "true");
-        System.setProperty("webdriver.chrome.driver","/usr/bin/google-chrome");
+        System.setProperty("webdriver.chrome.driver",
+                System.getProperty("user.dir") + "/src/test/resources/webdrivers/chromedriver.exe");
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
         chromeOptions.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
         chromeOptions.addArguments("use-fake-device-for-media-stream");
         chromeOptions.addArguments("use-fake-ui-for-media-stream");
         chromeOptions.addArguments("--disable-notifications");
-        chromeOptions.addExtensions(new File(System.getProperty("user.dir") + "\\src\\test\\resources\\ChromeExtension\\1.6.8_0.crx"));
+        chromeOptions.addExtensions(new File(System.getProperty("user.dir") + "/src/test/resources/ChromeExtension/1.6.8_0.crx"));
         chromeOptions.addArguments("--no-sandbox");
         HashMap<String, Object> prefs = new HashMap<String, Object>();
         prefs.put("profile.default_content_settings.popups", 0);
