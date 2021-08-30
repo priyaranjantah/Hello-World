1187887203a1a54106744f4e1582f86179038553
priyaranjantah <priyaranjantah@gmail.com>
2021-08-30 22:44:54 +0530

    updated

--- src/test/java/entrypoint/ChromeExten.java
+++ src/test/java/entrypoint/ChromeExten.java
@@ -5,12 +5,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.sql.SQLOutput;
 import java.util.HashMap;
+
+import static org.testng.AssertJUnit.assertTrue;
 
 public class ChromeExten {
 
@@ -19,16 +23,14 @@ public class ChromeExten {
 
     @BeforeClass
     public void setUp() {
-        System.setProperty("webdriver.chrome.silentOutput", "true");
-        System.setProperty("webdriver.chrome.driver",
-                System.getProperty("user.dir") + "/src/test/resources/webdrivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
         chromeOptions.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
         chromeOptions.addArguments("use-fake-device-for-media-stream");
         chromeOptions.addArguments("use-fake-ui-for-media-stream");
         chromeOptions.addArguments("--disable-notifications");
-        chromeOptions.addExtensions(new File(System.getProperty("user.dir") + "/src/test/resources/ChromeExtension/1.6.8_0.crx"));
+        //chromeOptions.addExtensions(new File(System.getProperty("user.dir") + "\\src\\test\\resources\\ChromeExtension\\1.6.8_0.crx"));
         chromeOptions.addArguments("--no-sandbox");
         HashMap<String, Object> prefs = new HashMap<String, Object>();
         prefs.put("profile.default_content_settings.popups", 0);
@@ -48,6 +50,8 @@ public class ChromeExten {
     @Test
     public void testScript() throws InterruptedException {
         driver.get("https://gethighlight.com/spa/home/accounts");
+        String title = driver.getTitle();
+        Assert.assertEquals(title,"Highlight");
         Thread.sleep(10000);
     }
 
